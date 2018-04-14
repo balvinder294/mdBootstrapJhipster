6ae53b66b8b400a71988cce8b8cfba0da529dd23
balvinder singh <balwinder.singh@simbalabs.com>
2018-04-14 17:24:12 +0530

    first commit

---
+++ navbars/logo.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'logo',
+  template: `
+  <ng-content></ng-content>
+  `
+})
+export class LogoComponent {
+
+}
